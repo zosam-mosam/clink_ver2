f980bc1f4d4780ab573e28b7029ddb4836169c3c
HyeyoungCho <HyeyoungCho97@gmail.com>
2023-07-10 11:31:26 +0900

    add mainCalendar

--- clink-react-app/src/component/MainCalendar.js
+++ clink-react-app/src/component/MainCalendar.js
@@ -1,15 +1,22 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Calendar from "react-calendar";
-import moment from "moment";
-
-import "../styles/MainCalendar.scss"
+import moment from "moment/moment";
+import "../styles/MainCalendar.scss";
 
 const MainCalendar = () => {
   const [value, onChange] = useState(new Date());
 
   return (
     <div>
-      <Calendar onChange={onChange} value={value} />
+      <Calendar
+        onChange={onChange}
+        value={value}
+        formatDay={(locale, date) => moment(date).format("DD")} // 날'일' 제외하고 숫자만 보이도록 설정
+        minDetail="month"
+        maxDetail="month"
+        showNeighboringMonth={false} //전 달 || 다음 달 일자 보이기
+        selectRange={true}
+      />
     </div>
   );
 };
