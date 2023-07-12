0403f4828029a6da50e7843487f03cb9ff378063
Nohkki <jin0201948@gmail.com>
2023-07-12 17:28:53 +0900

    add: 회원가입 서버연결, 로그인 서버연결중

--- clink-react-app/src/components/Join.js
+++ clink-react-app/src/components/Join.js
@@ -1,82 +1,157 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/Join.scss';
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-};
-function joinHandler(){
-    console.log("전송");
-}
-
 const Join = () => {
+  // 아이디 중복체크
+
+  // 모든 칸 입력했는지 체크
+
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState('');
+  const [userId, setuserId] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [confirmPwd, setConfirmPwd] = useState('');
+  const [email, setEmail] = useState('');
+
+  function handleSubmit(e) {
+    // e.preventDefault();
+
+    var param = {
+      userName: userName,
+      userId: userId,
+      nickname: nickname,
+      pwd: pwd,
+      confirmPwd: confirmPwd,
+      email: email,
+    };
+
+    axios
+      .post('http://localhost:80/user/join.do', param)
+      .then((response) => {
+        // console.log(response);
+        window.alert('회원가입 되었습니다. 로그인해주세요.');
+        console.log(response.data);
+        if (response.data === 'yes') {
+          navigate('/login');
+        }
+        // 아닐 때 입력창 빈칸만들기?
+      })
+      .catch((error) => {
+        console.log(error);
+        window.alert('다시 시도해세요');
+      });
+  }
+
+  // function handleSubmit(e) {
+  //   // 회원가입
+  //   e.preventDefault();
+  //   console.log('전송');
+  //   const form = e.target;
+  //   const formData = new FormData(form);
+  //   axios
+  //     .post(baseUrl, formData)
+  //     .then(function (response) {
+  //       console.log('회원가입 성공:', response);
+  //     })
+  //     .catch(function (error) {
+  //       console.error('회원가입 실패:', error);
+  //     });
+  // }
+
   return (
     <div className="JoinContainer">
       <div id="backgroundCircle"></div>
-      <form action='join.do' method='post' onSubmit={handleSubmit}>
+      <form action="join.do" method="post">
         <div className="JoinTitle">
           <h1>회원가입</h1>
         </div>
         <div className="JoinInputBox">
           <Form.Control
-            type="text"
             name="name"
             placeholder="이름"
             className="joinInput"
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
           />
-           <InputGroup className="joinInput">
+          <InputGroup className="joinInput">
             <Form.Control
-              name="id" placeholder="아이디"   
+              name="userId"
+              placeholder="아이디"
+              variant="outline-secondary"
+              onChange={(e) => {
+                setuserId(e.target.value);
+              }}
             />
-            <Button variant="outline-secondary" id="JoinIdentifyBtn"
-            >
-              중복확인
-            </Button>
+            <Button id="JoinIdentifyBtn">중복확인</Button>
           </InputGroup>
           <Form.Control
-            type="text"
             name="nickname"
             placeholder="닉네임"
             className="joinInput"
+            onChange={(e) => {
+              setNickname(e.target.value);
+            }}
           />
           <Form.Control
             type="password"
             name="password"
             placeholder="비밀번호"
             className="joinInput"
+            onChange={(e) => {
+              setPwd(e.target.value);
+            }}
           />
           <Form.Control
             type="password"
             name="confirmPassword"
             placeholder="비밀번호 확인"
             className="joinInput"
+            onChange={(e) => {
+              setConfirmPwd(e.target.value);
+            }}
           />
-          <Form.Control
+          {/* <Form.Control
             type="email"
             name="email"
-            placeholder="이메일"
             className="joinInput"
-          />
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          /> */}
           <InputGroup className="joinInput">
             <Form.Control
-              placeholder="휴대폰 본인인증"  
+              placeholder="이메일"
+              type="email"
+              name="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
             />
-            <Button variant="outline-secondary" id="JoinIdentifyBtn"
-            >
+            <Button variant="outline-secondary" id="JoinIdentifyBtn">
               본인인증하기
             </Button>
           </InputGroup>
         </div>
       </form>
       <div className="JoinBtnBox">
-        <Button variant="primary" className="LoginSubmitBtn" type="submit" onClick={joinHandler}>
+        <Button
+          variant="primary"
+          className="LoginSubmitBtn"
+          type="submit"
+          onClick={() => handleSubmit()}
+        >
           회원가입하기
         </Button>
       </div>
-      <div className='JoinLoginBtn'>로그인</div>
+      <div className="JoinLoginBtn">로그인</div>
     </div>
   );
 };
--- clink-react-app/src/components/Login.js
+++ clink-react-app/src/components/Login.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import pig from '../assets/pig.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/Login.scss';
-import { Link } from 'react-router-dom';
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-};
-// function loginHandler(){
-//     console.log("idpwd전송");
-// }
 const Login = () => {
+  const navigate = useNavigate();
+  const [userId, setuserId] = useState('');
+  const [pwd, setPwd] = useState('');
+
+  const handleLoginSubmit = () => {
+    // e.preventDefault();
+    //     console.log("idpwd전송");
+
+    let param = {
+      userId: userId,
+      pwd: pwd,
+    };
+
+    axios
+      .post('http://localhost:80/user/login.do', param)
+      .then((response) => {
+        // console.log(response);
+        window.alert('로그인되었습니다.');
+        console.log(response.data);
+        if (response.data === 'success') {
+          navigate('/');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        window.alert('다시 시도해세요');
+      });
+  };
+
   return (
     <div className="LoginContainer">
       <div className="LoginImage">
@@ -21,13 +46,23 @@ const Login = () => {
       <div className="LoginTitle">
         <h2>로그인</h2>
       </div>
-      <form action="login.do" method="post" onSubmit={handleSubmit}>
+      <form action="login.do" method="post">
         <div className="LoginForm">
-          <Form.Control type="text" id="inputPassword5" placeholder="아이디" />
+          <Form.Control
+            type="text"
+            id="inputPassword5"
+            placeholder="아이디"
+            onChange={(e) => {
+              setuserId(e.target.value);
+            }}
+          />
           <Form.Control
             type="password"
             id="inputPassword5"
             placeholder="비밀번호"
+            onChange={(e) => {
+              setPwd(e.target.value);
+            }}
           />
         </div>
       </form>
@@ -40,7 +75,12 @@ const Login = () => {
         </button>
       </div>
       <div className="LoginButtonBox">
-        <Button variant="primary" className="LoginSubmitBtn" type="submit">
+        <Button
+          variant="primary"
+          className="LoginSubmitBtn"
+          type="submit"
+          onClick={() => handleLoginSubmit()}
+        >
           로그인하기
         </Button>
       </div>
