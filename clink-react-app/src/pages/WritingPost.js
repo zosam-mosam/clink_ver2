83ad4463f3d62d6afc601d2fc4e69eb21659b392
soieu <soieu9898@gmail.com>
2023-07-17 10:36:33 +0900

    Add writingpost 수정

--- clink-react-app/src/pages/WritingPost.js
+++ clink-react-app/src/pages/WritingPost.js
@@ -13,7 +13,9 @@ export default function WritingPost() {
       <WritingCategory></WritingCategory>
       <Form>
         <Form.Group className="" controlId="formPost">
-          <Form.Control as="textarea" rows={15} />
+          <Form.Control type="text" placeholder="제목을 입력하세요" />
+          <br />
+          <Form.Control as="textarea" rows={13} placeholder="내용을 입력하세요" />
         </Form.Group>
       </Form>
       <PostTagInput></PostTagInput>
