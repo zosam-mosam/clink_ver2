f9f232275f8d149a3ce43e5f5da2f7a0684eaa9f
HyeyoungCho <HyeyoungCho97@gmail.com>
2023-07-17 14:48:14 +0900

    230717 은기언니꺼랑 머지전

--- clink-react-app/src/pages/Challenge.js
+++ clink-react-app/src/pages/Challenge.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import '../styles/Challenge.scss';
-import ChallengeTitle from '../components/Challenge/ChallengeTitle';
-import ChallengeGoal from '../components/Challenge/ChallengeGoal';
-import Header from '../components/Header';
-import ChallengeGraph from '../components/Challenge/ChallengeGraph';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../styles/Challenge.scss";
+import ChallengeTitle from "../components/Challenge/ChallengeTitle";
+import ChallengeGoal from "../components/Challenge/ChallengeGoal";
+import Header from "../components/Header";
+import ChallengeGraph from "../components/Challenge/ChallengeGraph";
 
 const Challenge = () => {
+  const [ChallengeTitleText, setChallengeTitleText] = useState();
+  const [ChallengeDescriptionText, setChallengeDescruotuibText] = useState();
+  const [max, setMax] = useState();
+  const [value, setValue] = useState();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:80/challenge/index.do?userNo=1")
+      .then((response) => {
+        console.log(response);
+        setChallengeTitleText(response.data.title);
+        setChallengeDescruotuibText(response.data.description);
+        setMax(response.data.goal);
+        setValue(response.data.value);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
   return (
-    <div className="Challenge" style={{ paddingBottom: '20%' }}>
+    <div className="Challenge" style={{ paddingBottom: "20%" }}>
       <div id="backgroundCircle"></div>
       <div className="challengContent">
         <Header />
-        <ChallengeTitle />
-        <ChallengeGoal value={7200} max={10000} />
+        <ChallengeTitle
+          title={ChallengeTitleText}
+          description={ChallengeDescriptionText}
+        />
+        <ChallengeGoal value={value} max={max} />
         <ChallengeGraph />
       </div>
     </div>
--- clink-react-app/src/pages/Login.js
+++ clink-react-app/src/pages/Login.js
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import pig from '../assets/pig.png';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/Login.scss';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import pig from "../assets/pig.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/Login.scss";
 
 const Login = () => {
   const navigate = useNavigate();
-  const [userId, setuserId] = useState('');
-  const [pwd, setPwd] = useState('');
+  const [userId, setuserId] = useState("");
+  const [pwd, setPwd] = useState("");
 
   const handleLoginSubmit = () => {
     // e.preventDefault();
     //     console.log("idpwd전송");
 
-    if (userId.trim() === '' || pwd.trim() === '') {
-      setuserId('');
-      setPwd('');
+    if (userId.trim() === "" || pwd.trim() === "") {
+      setuserId("");
+      setPwd("");
       console.log(userId);
-      alert('아이디 또는 패스워드가 공백입니다');
+      alert("아이디 또는 패스워드가 공백입니다");
     } else {
       var param = {
         userId: userId,
@@ -29,21 +29,25 @@ const Login = () => {
       };
       console.log(userId, pwd);
       axios
-        .post('http://localhost:80/clink/user/login.do', param)
+        .post("http://localhost:80/clink/user/login.do", param)
         .then((response) => {
           console.log(response.data);
-          if (response.data === 'success') {
-            alert('로그인되었습니다.');
-            navigate('/mypage');
+
+          if (response.data) {
+            sessionStorage.setItem("userId", response.data.userId);
+            // sessionStorage.setItem('userId', response.data.userId);
+            // sessionStorage.setItem('userId', response.data.userId);
+            alert(sessionStorage.getItem("userId") + " 로그인되었습니다.");
+            navigate("/mypage");
           } else {
-            alert('다시 시도하세요');
-            setuserId('');
-            setPwd('');
+            alert("다시 시도하세요");
+            setuserId("");
+            setPwd("");
           }
         })
         .catch((error) => {
           console.log(error);
-          alert('다시 시도하세요');
+          alert("다시 시도하세요");
         });
     }
   };
