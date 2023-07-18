8d603e4a25c8ad54244cedbd8879f693e9db2e21
zeegeon <zeegeon@naver.com>
2023-07-18 12:36:01 +0900

    Merge Mainpage

--- clink-react-app/src/components/MainHello.js
+++
@@ -1,5 +0,0 @@
-const MainHello = ({ name }) => {
-  return <div className="main-hello">저축 초보자 {name}님 안녕하세요 !</div>;
-};
-
-export default MainHello;
--- clink-react-app/src/components/main/MainSavingTotal.js
+++ clink-react-app/src/components/main/MainSavingTotal.js
@@ -1,42 +1,58 @@
-import SavingBackgroundImage from '../../images/main_coin.svg';
+import { useState } from "react";
+import SavingBackgroundImage from "../../images/main_coin.svg";
 
-const MainSavingTotal = ({ saving }) => {
-  // value 하나 두고 1이면 {어제} {전체} 자리 바꿀지
+const MainSavingTotal = ({ saving, totalSave }) => {
+  let dayflag = 1;
+  const [a, setA] = useState("어제");
+  const [b, setB] = useState("전체");
+  const [amount, setAmount] = useState(totalSave - saving);
+
+  const handleClickButton = (e) => {
+    const target = e.target.parentNode;
+    target.classList.toggle("main-card-rotate");
+    setTimeout(() => {
+      if (dayflag === 1) {
+        dayflag = 0;
+        setA("전체");
+        setB("어제");
+        setAmount(totalSave);
+      } else {
+        dayflag = 1;
+        setA("어제");
+        setB("전체");
+        setAmount(totalSave - saving);
+      }
+    }, 1500);
+  };
+
   return (
     <div
       className="main-savingsYesterday"
-      style={{
-        backgroundImage: 'url(' + SavingBackgroundImage + ')',
-        // boxShadow: '3px 3px 3px 3px gray',
-      }}
-      onClick={(e) => {
-        const target = e.target.parentNode;
-        target.classList.toggle('main-card-rotate');
-        // useState 로 값 변경 conponent
-      }}
+      style={{ backgroundImage: "url(" + SavingBackgroundImage + ")" }}
+      onClick={handleClickButton}
     >
-      <p style={{ textAlign: 'left', padding: 10, height: '5%' }}>
-        {'어제'} 저축 금액 {'\n'}
+      <p style={{ textAlign: "left", padding: 10, height: "5%" }}>
+        {a} 저축 금액 {"\n"}
       </p>
       <p
         style={{
-          textAlign: 'left',
+          textAlign: "left",
           padding: 10,
-          height: '50%',
-          fontSize: '50px',
+          height: "50%",
+          fontSize: "50px",
         }}
       >
-        {saving.toLocaleString()}원 {'\n'}
+        {amount.toLocaleString()}원 {"\n"}
       </p>
       <p
         style={{
-          textAlign: 'right',
+          textAlign: "right",
           padding: 10,
-          height: '5%',
-          fontSize: '10px',
+          height: "5%",
+          fontSize: "10px",
         }}
       >
-        {'전체'} 저축액을 알고 싶으면 클릭해주세요 !
+        {b} 저축액을 알고 싶으면 클릭해주세요 !
       </p>
     </div>
   );
